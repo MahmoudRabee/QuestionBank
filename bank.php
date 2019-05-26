25d31e0b7560c894e88e9351879b2af004118202
Mahmoud <mrabee63@gmail.com>
2019-05-26 04:18:12 +0200

    add studnt loging and register and complete student reg

--- bank.php
+++ bank.php
@@ -2,5 +2,22 @@
 include_once  'connectDB.php' ; #connect database
 include 'creatTables.php' ; 	# creat tables 
 
+echo '
+<body>
+	<h1>Welcom in Question Bank</h1>
+	<p>This is website you can take an online auto generated exam </p>
+
+<form action="test.html">
+  <input type="submit" value="Student">
+</form>
+<form action="test.php">
+  <input type="submit" value="doctor">
+</form>
+
+</body>
+
+';
+
+
 
 ?>
---
+++ studentReg.php
@@ -0,0 +1,64 @@
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+  Name:<br>
+  <input type="text" name="name" value="Name"><br>
+  ID:<br>
+  <input type="text" name="id" value="ID"><br><br>
+  Grade:<br>
+  <select name="Grade">
+  <option value="power">power</option>
+  <option value="communication">communication</option>
+  <option value="computer">computer</option>
+</select>
+  <input type="submit" value="Regisrer">
+</form>
+
+
+<?php
+if ($_SERVER['REQUEST_METHOD']=='POST'){
+	include  'connectDB.php' ; #connect database
+
+if (checkid($_POST['id'],$db)){
+register($_POST['name'] , $_POST['id'] , $_POST['Grade'],$db);
+echo '
+	<form action="studentLogin.php">
+	You have created an account successfully 
+  		<input type="submit" value="Login">
+	</form>
+';
+
+} else {
+	echo "this student ID is allready exist"."<br>";
+}
+}
+
+
+function register($name , $id , $grade ,$db){
+try {
+		  
+    // our SQL statements
+    $db->exec("INSERT INTO Students (Student_id, Name, Grade ) 
+    VALUES ('".$id."','".$name."','".$grade."')");   
+	}
+	catch (PDOException $e){
+
+		echo 'Failed' . $e->getMessage();
+	}
+}
+
+
+function checkid( $value,$db)
+{
+	$tableName = 'Students' ;
+	$prop      = 'Student_id' ; 
+	$columnName= 'Student_id';
+    $q = $db->query("SELECT `$columnName` FROM `$tableName` WHERE $prop='".$value."'");
+    $f = $q->fetch();
+    $result = $f[$columnName];
+
+    if (is_null($result)){
+    	return true ; 
+    } else {
+    	return false ; 
+    }
+}
+?>
